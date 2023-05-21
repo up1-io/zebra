db6e5cf3c200deed8f25998919598d36908e8a2a
Jonathan Mausbach <mausbach@up1.io>
2023-05-21 21:13:26 +0200

    added zebra documentation

--- zebra.go
+++ zebra.go
@@ -1,25 +1,30 @@
+// Package zebra is a web framework for Go. It is designed to be simple and easy to use.
 package zebra
 
 import (
 	"path/filepath"
 )
 
+// Zebra is the main struct of the framework.
 type Zebra struct {
 	RootDir string
 	Pages   []Page
 	Router  Router
 }
 
+// Option is a function that can be passed to New to configure the Zebra instance.
 type Option func(*Zebra)
 
 const defaultRootDir = "."
 
+// WithRootDir sets the root directory of the project.
 func WithRootDir(dir string) Option {
 	return func(z *Zebra) {
 		z.RootDir = dir
 	}
 }
 
+// New creates a new Zebra instance.
 func New(opt ...Option) (*Zebra, error) {
 	r := NewRouter()
 	z := &Zebra{
