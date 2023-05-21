f4d06ab93c60d4f7b53cdbe3301096a81787c02c
Jonathan Mausbach <mausbach@up1.io>
2023-05-21 21:44:58 +0200

    extracted package doc to own file

--- zebra.go
+++ zebra.go
@@ -1,15 +1,3 @@
-// Package zebra is a web framework for Go. It is designed to be simple and easy to use.
-//
-// To get started, create a new Zebra instance and serve it:
-//
-//	func main() {
-//			app, err := zebra.New()
-//			if err != nil {
-//				log.Fatalln(err)
-//			}
-//
-//			log.Fatalln(app.ListenAndServe(":8080"))
-//	}
 package zebra
 
 import (
