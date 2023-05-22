2609e38777d7431612baa6c304ab273fd6401594
Jonathan Mausbach <mausbach@up1.io>
2023-05-22 08:26:44 +0200

    moved examples to v2 folder

---
+++ examples_v2/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"github.com/up1-io/zebra"
+	"log"
+)
+
+// ToDo: reduce tailwindcss size
+// ToDo: add component support
+
+func main() {
+	app, err := zebra.New()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	app.Router.On("/about", aboutHandler)
+	app.Router.On("/", homeHandler)
+	app.Router.On("/users/{id}/", userHandler)
+	app.Router.On("/users/{id}/details/{postId}", postHandler)
+	app.Router.On("/test/redirect-test/", redirectHandler)
+
+	log.Fatalln(app.ListenAndServe(":8080"))
+}
+
+func aboutHandler(ctx zebra.Request, callback zebra.Callback) {
+	callback(nil, zebra.Result{
+		Data: map[string]interface{}{
+			"Content": struct {
+				Title string
+			}{
+				Title: "Dynamic title set on server side",
+			},
+		},
+	})
+}
+
+func homeHandler(ctx zebra.Request, callback zebra.Callback) {
+	callback(nil, zebra.Result{
+		Data: map[string]interface{}{
+			"Content": struct {
+				Status string
+			}{
+				Status: "Hello World",
+			},
+		},
+	})
+}
+
+func userHandler(r zebra.Request, callback zebra.Callback) {
+	callback(nil, zebra.Result{
+		Data: map[string]interface{}{
+			"Content": struct {
+				Id string
+			}{
+				Id: r.PathVariables["id"],
+			},
+		},
+	})
+}
+
+func postHandler(r zebra.Request, callback zebra.Callback) {
+	callback(nil, zebra.Result{
+		Data: map[string]interface{}{
+			"Content": struct {
+				Id     string
+				PostId string
+			}{
+				Id:     r.PathVariables.Get("id"),
+				PostId: r.PathVariables.Get("postId"),
+			},
+		},
+	})
+}
+
+func redirectHandler(r zebra.Request, callback zebra.Callback) {
+	callback(nil, zebra.Result{
+		Redirect: "/",
+	})
+}
